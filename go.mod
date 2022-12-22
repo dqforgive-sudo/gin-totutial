6abbebe4867a438790cc065be2169099fa25569a
dongqian <dongqian@qianxin.com>
2022-12-22 22:59:25 +0800

    add gin

---
+++ go.mod
@@ -0,0 +1,13 @@
+module gin_tutorial
+
+go 1.16
+
+require (
+	github.com/astaxie/beego v1.12.3
+	github.com/gin-gonic/gin v1.8.1
+	github.com/go-ini/ini v1.67.0
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
+	github.com/unknwon/com v1.0.1
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"gin_tutorial/routers"
+	"net/http"
+
+	"gin_tutorial/pkg"
+)
+
+func main() {
+	router := routers.InitRouter()
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
+}
